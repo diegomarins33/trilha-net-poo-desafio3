526b52aff41873fc144531df6aee5a4b3b6b4da5
diegomarins33 <diegomarins33@gmail.com>
2024-08-22 21:18:02 -0300

    challenge-solution

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Smartphone n1 = new Nokia("111111111", "Nokia n1", "0909090909", 128);
+Smartphone i1 = new Iphone("222222222", "Iphone i1", "080808080", 64);
+
+n1.Ligar();
+n1.ReceberLigacao();
+n1.InstalarAplicativo("Instagram");
+System.Console.WriteLine("---------------");
+i1.Ligar();
+i1.ReceberLigacao();
+i1.InstalarAplicativo("Instagram");
